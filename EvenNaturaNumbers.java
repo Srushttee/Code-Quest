8089c64c6be95b9a46952391117685f8567ca1b1
Srushttee <srushtitatte66@gmail.com>
2025-12-09 20:01:12 +0530

    Day 1

---
+++ EvenNaturaNumbers.java
@@ -0,0 +1,11 @@
+public class EvenNaturaNumbers {
+    public static void main(String[] args) 
+    {
+        for(int i=2;i<=100;i+=2)
+        {
+            System.err.println(i);
+        }
+    
+    }
+    
+}
---
+++ OddNaturalNumbers.java
@@ -0,0 +1,9 @@
+public class OddNaturalNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <=99; i+=2) {
+            System.err.println(i);
+            
+        }
+    }
+    
+}
